eb1bbcfe5e831d5757cd0cd435e980ff0c3fdb2b
'T.J. <c0640785@mylambton.ca>
2015-09-14 16:05:53 -0400

    Testing?

---
+++ src/javaassignment1/pkg2015/JavaAssignment12015.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment1.pkg2015;
+
+/**
+ *
+ * @author c0640785
+ */
+public class JavaAssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        
+        /***************************************
+         * Assignment #1
+         * 
+         * Job Posting: Java Developer
+         * http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Back-End-Java-Developer-be6b886816880859?q=Java
+         * 
+         * Java EE - Java Enterprise Edition https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Spring Framework - Application Framework https://en.wikipedia.org/wiki/Spring_Framework
+         * PIG - A high-level data-flow language and execution framework for parallel computation.  https://en.wikipedia.org/wiki/Pig_(programming_tool)
+         * 
+         * Toronto Salary- $78K
+         * Vancouver Salary- $82K
+         * Waterloo Salary - $80K
+         * 
+         */
+    }
+    
+}
